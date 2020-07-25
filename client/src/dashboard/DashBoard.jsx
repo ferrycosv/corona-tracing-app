a0d58757d5810fdae0c24387ff2706a665ce0ada
khalender <m_nuralp@gitlab.com>
2020-07-26 01:16:15 +0200

    dashboard styling : * grid design is ready * listing component is ready

--- client/src/dashboard/DashBoard.jsx
+++ client/src/dashboard/DashBoard.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import ListingComponent from './Listing';
 import styles from './dashboard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faSpinner, faUserCircle, faHome } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function withAuth(ComponentToProtect) {
     return class extends Component {
@@ -44,9 +47,24 @@ export default function withAuth(ComponentToProtect) {
 
                 <div className={[styles.container, styles.border].join(" ")}>
 
-                    <div className={[styles.leftSideBar, styles.border].join(" ")}>LeftSidebar</div>
-                    <div className={[styles.rightSideBar, styles.border].join(" ")}>
-                        <ListingComponent></ListingComponent>
+                    <div className={[styles.leftSideBar, styles.border, styles.center].join(" ")}>
+                        <div className="text-center text-light mb-3"><FontAwesomeIcon icon={faUserCircle} size="6x" /></div>
+                        <div className={styles.leftlinks}>
+                            <span><FontAwesomeIcon icon={faHome} size="1x" /></span>&nbsp;&nbsp;&nbsp;
+                            <span><a href="#" >home</a></span>
+                        </div>
+
+                    </div>
+                    <div className={styles.rightSideBar}>
+                        <div className="mt-5"><span style={{ fontSize: "16px", color: "#FFFFFF",fontWeight:600 }}>Contact List</span></div>
+                        <div className="mt-4">
+                            <span className={styles.listingButtons} style={{marginRight: "10px"}}>Last Ten Days</span>
+                            <span className={styles.listingButtons}>Last Month</span>
+                        </div>
+                        <div className="w-100">
+                            <ListingComponent></ListingComponent>
+                        </div>
+                        
                     </div>
                 </div>
 
