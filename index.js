6c3ec5dc9863c689025fce136419c47ff6fbed3c
Francisco Fernández <ferrycosv@gmail.com>
2020-08-12 03:34:11 +0200

    Modified the app layout to make it responsive

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 // this is the main entry point for your full app
 // it serves your frontend & provides access to your API
-/*
-IMPORTANT!!!
 
-YOU MUST SET THIS FLAG TO ENABLE FOR CHROME TO ALLOW HTTPS REQUEST TO API DURING DEVELOPMENT
-chrome://flags/#allow-insecure-localhost
-*/
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config");
 const api = require("./api/");
-/*const fs = require("fs");
-const https = require("https");*/
 
 const app = express();
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -47,17 +40,3 @@ app.listen(config.PORT, () => {
     `listening at http://localhost:${config.PORT} (${config.MODE} mode)`
   );
 });
-/*https
-  .createServer(
-    {
-      key: fs.readFileSync("./server.key"),
-      cert: fs.readFileSync("./server.cert"),
-    },
-    app
-  )
-  .listen(config.PORT, () =>
-    console.log(
-      `listening at https://localhost:${config.PORT} (${config.MODE} mode)`
-    )
-  );*/
-
